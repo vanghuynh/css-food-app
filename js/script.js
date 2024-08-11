0282fd6a3dfd79f848e79c0fb500d5ced3f1a93f
Vang Huynh <huynh.vang@devexperts.com>
2024-08-11 09:57:18 +0700

    adding icon and meta data

--- js/script.js
+++ js/script.js
@@ -55,6 +55,31 @@ allLinks.forEach(function (link) {
   });
 });
 
+// sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      console.log(ent);
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      console.log(ent);
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
